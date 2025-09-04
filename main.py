46c9cb5e61c53428c347537b18db5ac04fedbcaa
Abhishek Kohli <ab0210211@gmail.com>
2025-09-04 20:56:07 +0100

    chapter 3 complete

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+# main function
 def main():
-    print("Hello from aiagent!")
+  
+  
+  load_dotenv()
+  api_key = os.environ.get("GEMINI_API_KEY")
+  
+  
+  # create client
+  client = genai.Client(api_key=api_key)
+  
+  response = client.models.generate_content(
+      model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+  )
+  
+  print(response.text)
+  
+  if response is None or response.usage_metadata is None:
+    print("No usage metadata available.")
+    return
+  print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+  print(f"Response tokens:{response.usage_metadata.candidates_token_count}" )
+  print("ARGS", sys.argv)
+  
 
 
-if __name__ == "__main__":
-    main()
+main()
