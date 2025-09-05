56cdf67bdc712be28eebd57b779d67ce2450c9f4
Abhishek Kohli <ab0210211@gmail.com>
2025-09-05 23:03:04 +0100

    section2, chapter 3  Done

--- tests.py
+++ tests.py
@@ -1,16 +1,24 @@
-from functions.get_files_info import get_files_info
+# from functions.get_files_info import get_files_info
+from functions.get_files_content import get_file_content
 import os
 
-def main():
-    working_dir ="calculator"
-    get_files_info(working_dir)
-    root_content = get_files_info(working_dir)
-    print(root_content)
-    pkg_contents = get_files_info(working_dir,"pkg")
-    print(pkg_contents)
-    bin_contents = get_files_info(working_dir,"/bin")
-    print(bin_contents)
-    out_of_dir = get_files_info(working_dir,"../")
-    print(out_of_dir)
-    # print(os.path.isdir(working_dir))
+def main():   
+    # working_dir = "calculator"
+    # print(get_file_content(working_dir, "lorem.txt"))
+    # print(get_file_content("calculator", "lorem.txt"))
+    
+    result = get_file_content("calculator", "main.py")
+    print(result)
+    
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print(result)
+    
+    result = get_file_content("calculator", "/bin/cat") 
+    print(result)
+
+    result = get_file_content("calculator", "pkg/does_not_exist.py") 
+    print(result)
+    
+    
+    
 main()
